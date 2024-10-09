a5e0b73bcd2c399b403bb61fd4caeea993fbd1c2
Heather Truong <heath9r@gmail.com>
2024-10-09 11:36:03 -0400

    updated header using React

---
+++ my-react-app/src/Header.jsx
@@ -0,0 +1,21 @@
+function Header() {
+    return (
+        <div>
+            <div class="banner">
+                <img id="main-photo" src="/LanaDelReyPic.png" alt="Picture of Lana Del Rey"></img>
+
+                <div id="banner-right">
+                    <h1 class="oswald-title">LANA DEL REY</h1>
+                </div>
+            </div>
+
+            <div class="nav-bar">
+                <h2 class="center oswald-header">HOME</h2>
+                <h2 class="center oswald-header">SONG RANKINGS</h2>
+                <h2 class="center oswald-header">TOUR DATES</h2>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
